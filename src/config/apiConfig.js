a2a22a497e4a11692cef81aaedba245801690d0d
Ever Zamora <ever.zamora@catolica.edu.sv>
2025-11-09 08:51:32 -0600

    z

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -3,7 +3,7 @@
 // export const LARAVEL_BASE = 'http://localhost:5000';
 // Para usar Spring Boot (ejemplo local en puerto 8080), descomentar la línea correspondiente
 //export const SPRING_BASE = "http://localhost:8080";
-export const LARAVEL_BASE = 'https://apilaravel-production-9a74.up.railway.app/';
+export const LARAVEL_BASE = 'https://apilaravel-production-9a74.up.railway.app';
 // Selecciona aquí la base que quieras usar (comentar la otra)
 export const BASE_URL = LARAVEL_BASE;
 // export const BASE_URL = LARAVEL_BASE;
